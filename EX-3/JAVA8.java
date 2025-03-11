de9aedd9f830116ad046b84efea1aaf14e7bd2e8
Vighranth-Kumarasamy <vighranth.s.k@gmail.com>
2025-03-12 00:57:15 +0530

    Add files via upload

---
+++ EX-3/JAVA8.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        boolean isPrime = true;
+
+        if (num < 2) isPrime = false;
+        else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(num + (isPrime ? " is Prime" : " is not Prime"));
+        scanner.close();
+    }
+}
